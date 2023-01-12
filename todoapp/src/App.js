ca848d49ee08e2a3125bdbe93c4ab33180be3885
Karim <aarrale12@gmail.com>
2023-01-12 17:30:17 -0600

    form

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,8 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import "./App.css";
+import Form from './components/Form';
 
-function App() {
-  return <div>App</div>
+const App = () => {
+
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  return(
+    <div className="container">
+      <div className="app-header">
+        <div>
+          <Header />
+        </div>
+        <div>
+          <Form
+          input={input}
+          setInput={setInput}
+          todos={todos}
+          setTodos={setTodos}
+           />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
